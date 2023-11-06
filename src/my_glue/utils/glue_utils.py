335f13168fb673a30a56d49c426799fa6994a974
liangzzz <liang-shan@outlook.com>
2023-11-06 19:52:49 +0900

    update for windows

--- src/my_glue/utils/glue_utils.py
+++ src/my_glue/utils/glue_utils.py
@@ -40,9 +40,9 @@ def load_df_from_s3(
     Args:
         context (GlueContext): The Glue context object.
         s3_path (str): The S3 Full path. s3://bucket/path or [s3://bucket/path, s3://bucket/path]
-        format_options (dict): The format options, defaults to csv
         format (str): The format of the file, defaults to csv
         schema (str): The schema
+        options (Dict[str, Any]): The options
     Returns:
         DataFrame: The resulting `DataFrame`.
     """
@@ -63,6 +63,8 @@ def load_df_from_s3_csv_with_schema(
     Args:
         context (GlueContext): The Glue context object.
         s3_path (str): The S3 path.
+        schema (StructType): The schema.
+        options (Dict[str, Any]): The options
     Returns:
         DataFrame: The resulting `DataFrame`.
     """
@@ -79,36 +81,6 @@ def load_df_from_s3_text(context: GlueContext, s3_path: str) -> DataFrame:
         DataFrame: The resulting `DataFrame`.
     """
     return context.spark_session.read.text(s3_path)
-
-
-def convert_data_frame_to_dynamic_frame(context: GlueContext, df: DataFrame, name: str) -> DynamicFrame:
-    """
-    Converts a specified pandas DataFrame, `df`, to a Glue DynamicFrame
-
-    Args:
-        context (GlueContext): The Glue context object.
-        df (DataFrame): The pandas DataFrame.
-        name (str): The name of the Glue DynamicFrame.
-
-    Returns:
-        DynamicFrame: The resulting Glue DynamicFrame
-    """
-    return DynamicFrame.fromDF(df, context, name)
-
-
-def convert_dynamic_frame_to_data_frame(context: GlueContext, df: DynamicFrame, name: str) -> DataFrame:
-    """
-    Converts a specified Glue DynamicFrame, `df`, to a pandas DataFrame
-
-    Args:
-        context (GlueContext): The Glue context object.
-        df (DynamicFrame): The Glue DynamicFrame.
-        name (str): The name of the pandas DataFrame.
-
-    Returns:
-        DataFrame: The resulting pandas DataFrame
-    """
-    return df.toDF(context, name)
 
 
 def export_data_frame_to_csv_dir(
