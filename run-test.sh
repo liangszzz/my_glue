b8a6fda7b16ca67f967efa2411d265efd8240e53
liangzzz <liang-shan@outlook.com>
2023-11-06 20:17:47 +0900

    sh

--- run-test.sh
+++ run-test.sh
@@ -1,1 +1,1 @@
-clear && poetry run pytest tests --cov-config=.coveragerc --cov=src/my_glue --cov-fail-under=100 --cov-report=xml:coverage/coverage.xml 
+poetry run pytest tests --cov-config=.coveragerc --cov=src/my_glue --cov-fail-under=100 --cov-report=xml:coverage/coverage.xml
